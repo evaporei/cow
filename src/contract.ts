248fc36851ba388ca24c50661e7c01a6efb1a438
Eva Pace <eba.pachi@gmail.com>
2022-09-27 00:11:21 -0300

    handle settle call

--- src/contract.ts
+++ src/contract.ts
@@ -1,68 +1,29 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, log, ethereum } from "@graphprotocol/graph-ts"
 import {
   Contract,
   Interaction,
   OrderInvalidated,
   PreSignature,
   Settlement,
-  Trade
+  Trade,
+  SettleCall,
 } from "../generated/Contract/Contract"
 import { ExampleEntity } from "../generated/schema"
 
-export function handleInteraction(event: Interaction): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+export function handleSettle(call: SettleCall): void {
+  let inputs = call.inputs;
+  let tokens = inputs.tokens;
+  for (let i = 0; i < tokens.length; i++) {
+    log.info(`token[${i}] = {}`, [tokens[i].toHexString()]);
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.target = event.params.target
-  entity.value = event.params.value
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.authenticator(...)
-  // - contract.domainSeparator(...)
-  // - contract.filledAmount(...)
-  // - contract.getStorageAt(...)
-  // - contract.preSignature(...)
-  // - contract.simulateDelegatecall(...)
-  // - contract.simulateDelegatecallInternal(...)
-  // - contract.vault(...)
-  // - contract.vaultRelayer(...)
+  let interactions = inputs.interactions;
+  log.info(`interactions.length {}`, [interactions.length.toString()]);
+  for (let i = 0; i < interactions.length; i++) {
+    log.info(`interactions[${i}].length {}`, [interactions[i].length.toString()]);
+    for (let j = 0; j < interactions[i].length; j++) {
+      let interaction = interactions[i][j];
+      log.info(`interaction[${i}][${j}] {}, {}, {}`, [interaction.target.toHexString(), interaction.value.toString(), interaction.callData.toString()])
+    }
+  }
 }
-
-export function handleOrderInvalidated(event: OrderInvalidated): void {}
-
-export function handlePreSignature(event: PreSignature): void {}
-
-export function handleSettlement(event: Settlement): void {}
-
-export function handleTrade(event: Trade): void {}
